732d619d7cd2da8596483863a39e6ebd0bab8668
Antony Nasce <nasce@mac.com>
2023-07-05 21:46:31 +0100

    Update with 3D bone cylinders!

--- LeapDemoSwift/HandPreviewViewController.swift
+++ LeapDemoSwift/HandPreviewViewController.swift
@@ -10,76 +10,6 @@ import Foundation
 import SwiftUI
 import SceneKit
 
-class CylinderLine: SCNNode
-{
-    init( parent: SCNNode,//Needed to add destination point of your line
-        v1: SCNVector3,//source
-        v2: SCNVector3,//destination
-        radius: CGFloat,//somes option for the cylinder
-        radSegmentCount: Int, //other option
-        color: NSColor )// color of your node object
-    {
-        super.init()
-
-        //Calcul the height of our line
-        let  height = v1.distance(receiver: v2)
-
-        //set position to v1 coordonate
-        position = v1
-
-        //Create the second node to draw direction vector
-        let nodeV2 = SCNNode()
-
-        //define his position
-        nodeV2.position = v2
-        //add it to parent
-        parent.addChildNode(nodeV2)
-
-        //Align Z axis
-        let zAlign = SCNNode()
-        zAlign.eulerAngles.x = CGFloat(Double.pi / 2.0)
-
-        //create our cylinder
-        let cyl = SCNCylinder(radius: radius, height: CGFloat(height))
-        cyl.radialSegmentCount = radSegmentCount
-        cyl.firstMaterial?.diffuse.contents = color
-
-        //Create node with cylinder
-        let nodeCyl = SCNNode(geometry: cyl )
-        nodeCyl.position.y = CGFloat(-height/2)
-        zAlign.addChildNode(nodeCyl)
-
-        //Add it to child
-        addChildNode(zAlign)
-
-        //set contrainte direction to our vector
-        constraints = [SCNLookAtConstraint(target: nodeV2)]
-    }
-
-    override init() {
-        super.init()
-    }
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-}
-
-private extension SCNVector3{
-    func distance(receiver:SCNVector3) -> Float{
-        let xd = receiver.x - self.x
-        let yd = receiver.y - self.y
-        let zd = receiver.z - self.z
-        let distance = Float(sqrt(xd * xd + yd * yd + zd * zd))
-
-        if (distance < 0){
-            return (distance * -1)
-        } else {
-            return (distance)
-        }
-    }
-}
-
-
 class HandPreviewViewController : NSObject, SCNSceneRendererDelegate {
     var leftHandSphere : SCNNode!
     var rightHandSphere : SCNNode!
@@ -92,9 +22,11 @@ class HandPreviewViewController : NSObject, SCNSceneRendererDelegate {
     
     var leftSpherePositions : [SCNVector3] = [SCNVector3]()
     var leftHandNodes : [SCNNode] = [SCNNode]()
+    var leftHandBoneNodes : [SCNNode] = [SCNNode]()
     
     var rightSpherePositions : [SCNVector3] = [SCNVector3]()
     var rightHandNodes : [SCNNode] = [SCNNode]()
+    var rightHandBoneNodes : [SCNNode] = [SCNNode]()
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         updateHandPositions()
@@ -151,38 +83,45 @@ class HandPreviewViewController : NSObject, SCNSceneRendererDelegate {
         }
         
         let leftHand = handManager.leftHand
-        let digits = leftHand?.digits
-        let thumb = digits!.0
-        let index = digits!.1
-        let middle = digits!.2
-        let ring = digits!.3
-        let pinky = digits!.4
-        let fingers = [thumb, index, middle, ring, pinky]
-        for fingerIx in 0...4 {
-            let finger = fingers[fingerIx]
-            let bones = [finger.bones.0, finger.bones.1, finger.bones.2, finger.bones.3]
-            for jointIx in 0...3
-            {
-                let index = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx)
-                let position = bones[jointIx].next_joint
-                let vec3 = SCNVector3(0.001*position.x, 0.001*position.y, 0.001*position.z)
-                leftSpherePositions[index] = vec3
-                UpdateSphereNodeWithPosition(node: leftHandNodes[index], position: vec3)
+        if let digits = leftHand?.digits {
+            let thumb = digits.0
+            let index = digits.1
+            let middle = digits.2
+            let ring = digits.3
+            let pinky = digits.4
+            let fingers = [thumb, index, middle, ring, pinky]
+            for fingerIx in 0...4 {
+                let finger = fingers[fingerIx]
+                let bones = [finger.bones.0, finger.bones.1, finger.bones.2, finger.bones.3]
+                for jointIx in 0...3
+                {
+                    let index = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx)
+                    let position = bones[jointIx].next_joint
+                    let vec3 = SCNVector3(0.001*position.x, 0.001*position.y, 0.001*position.z)
+                    leftSpherePositions[index] = vec3
+                    UpdateSphereNodeWithPosition(node: leftHandNodes[index], position: vec3)
+                }
             }
         }
+        else{
+            return;
+        }
         
-        //Draw cylinders between finger joints
-//        for fingerIx in 0...4 {
-//            for jointIx in 0...2
-//            {
-//                let keyA = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx)
-//                let keyB = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx + 1)
-//                if (keyA < leftCylinderNodes.count){
-//                    leftCylinderNodes[keyA] = CylinderLine(parent: leftHandSphere, v1: leftSpherePositions[keyA], v2: leftSpherePositions[keyB], radius: 0.01, radSegmentCount: 5, color: NSColor.white)
-//                    scene?.rootNode.addChildNode(leftCylinderNodes[keyA]])
-//                }
-//            }
-//        }
+        var leftBoneIndex = 0
+        //Draw cylinders between left finger joints
+        for fingerIx in 0...4
+        {
+            for jointIx in 0...2
+            {
+                let keyA = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx);
+                let keyB = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx + 1);
+                leftHandBoneNodes[leftBoneIndex] = leftHandBoneNodes[leftBoneIndex].buildLineInTwoPointsWithRotation(
+                    from: leftSpherePositions[keyA],
+                    to: leftSpherePositions[keyB],
+                    radius: SPHERE_RADIUS, color: .white)
+                leftBoneIndex += 1
+            }
+        }
     }
     
     func UpdateRightHandBonePositions(){
@@ -190,26 +129,46 @@ class HandPreviewViewController : NSObject, SCNSceneRendererDelegate {
             return
         }
         
-        let hand = handManager.rightHand
-        let digits = hand?.digits
-        let thumb = digits!.0
-        let index = digits!.1
-        let middle = digits!.2
-        let ring = digits!.3
-        let pinky = digits!.4
-        let fingers = [thumb, index, middle, ring, pinky]
-        for fingerIx in 0...4 {
-            let finger = fingers[fingerIx]
-            let bones = [finger.bones.0, finger.bones.1, finger.bones.2, finger.bones.3]
-            for jointIx in 0...3
-            {
-                let index = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx)
-                let position = bones[jointIx].next_joint
-                let vec3 = SCNVector3(0.001*position.x, 0.001*position.y, 0.001*position.z)
-                UpdateSphereNodeWithPosition(node: rightHandNodes[index], position: vec3)
+        let leftHand = handManager.rightHand
+        if let digits = leftHand?.digits {
+            let thumb = digits.0
+            let index = digits.1
+            let middle = digits.2
+            let ring = digits.3
+            let pinky = digits.4
+            let fingers = [thumb, index, middle, ring, pinky]
+            for fingerIx in 0...4 {
+                let finger = fingers[fingerIx]
+                let bones = [finger.bones.0, finger.bones.1, finger.bones.2, finger.bones.3]
+                for jointIx in 0...3
+                {
+                    let index = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx)
+                    let position = bones[jointIx].next_joint
+                    let vec3 = SCNVector3(0.001*position.x, 0.001*position.y, 0.001*position.z)
+                    rightSpherePositions[index] = vec3
+                    UpdateSphereNodeWithPosition(node: rightHandNodes[index], position: vec3)
+                }
             }
         }
+        else{
+            return;
+        }
         
+        var rightBoneIndex = 0
+        //Draw cylinders between left finger joints
+        for fingerIx in 0...4
+        {
+            for jointIx in 0...2
+            {
+                let keyA = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx);
+                let keyB = getFingerJointIndex(fingerIndex: fingerIx, jointIndex: jointIx + 1);
+                rightHandBoneNodes[rightBoneIndex] = rightHandBoneNodes[rightBoneIndex].buildLineInTwoPointsWithRotation(
+                    from: rightSpherePositions[keyA],
+                    to: rightSpherePositions[keyB],
+                    radius: SPHERE_RADIUS, color: .white)
+                rightBoneIndex += 1
+            }
+        }
     }
     
     
@@ -222,7 +181,6 @@ class HandPreviewViewController : NSObject, SCNSceneRendererDelegate {
     
     func InitialiseHandSpheres()
     {
-        
         leftSpherePositions = [SCNVector3](repeating: SCNVector3(), count: TOTAL_JOINT_COUNT)
         rightSpherePositions = [SCNVector3](repeating: SCNVector3(), count: TOTAL_JOINT_COUNT)
         
@@ -246,12 +204,26 @@ class HandPreviewViewController : NSObject, SCNSceneRendererDelegate {
             scene?.rootNode.addChildNode(leftHandNodes[nodeIx])
         }
         
-//        for boneIx in 0...14 {
-//            //let bone = DrawCylinder(positionA: leftHandNodes[boneIx].position, positionB: leftHandNodes[boneIx+1].position, inScene: scene)
-//            let bone = CylinderLine(parent: leftHandSphere, v1: leftHandNodes[boneIx].position, v2: leftHandNodes[boneIx+1].position, radius: 0.01, radSegmentCount: 5, color: NSColor.white)
-//            leftCylinderNodes.append(bone)
-//            scene?.rootNode.addChildNode(leftCylinderNodes[boneIx])
-//        }
+        // Initialise Left Hand Bone Cylinders
+        for boneIx in 0...24 {
+            let newBone = SCNNode()
+            leftHandBoneNodes.append(newBone)
+            scene.rootNode.addChildNode(
+                leftHandBoneNodes[boneIx].buildLineInTwoPointsWithRotation(
+                    from: SCNVector3(0.01,-0.01,0.03),
+                    to: SCNVector3(0.07,0.011,0.07),
+                    radius: SPHERE_RADIUS*0.8, color: .white))
+        }
+        // Initialise Right Hand Bone Cylinders
+        for boneIx in 0...24 {
+            let newBone = SCNNode()
+            rightHandBoneNodes.append(newBone)
+            scene.rootNode.addChildNode(
+                rightHandBoneNodes[boneIx].buildLineInTwoPointsWithRotation(
+                    from: SCNVector3(0.01,-0.01,0.03),
+                    to: SCNVector3(0.07,0.011,0.07),
+                    radius: SPHERE_RADIUS*0.8, color: .white))
+        }
         
         for nodeIx in 0...19{
             let sphere = SCNNode(geometry: sphereGeoRight)
